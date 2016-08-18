11b6bdba6cbfce9e836e8df0db6de2493ae6e786
cadhithya <cadhithya@gmail.com>
2016-08-18 23:59:25 +0530

    Create removelastnode.java

---
+++ removelastnode.java
@@ -0,0 +1,42 @@
+1) USING REMOVE LAST FUNCTION 
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+
+
+class codeChef {
+
+   public static void main(String[] args) {
+
+   
+
+    LinkedList list= new LinkedList();
+    
+    list.add(1);
+    list.add(2);
+    list.add(3);                // FOR MANUALLY ADDING VALUES TO LINKED LIST
+    list.add(4);
+    list.add(5);
+    
+    list.removeLast(); // java function to remove last node of a linked list
+    System.out.println(list);
+   }
+   
+   
+}
+
+
+
+
+
+
+ 
+
+
